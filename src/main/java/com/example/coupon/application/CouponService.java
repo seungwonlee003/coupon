fd9809593a4024d3784d7a2ad8ceebe97d209346
seungwonlee003 <seungwonlee03@icloud.com>
2024-04-10 08:29:36 +0900

    createCoupon

--- src/main/java/com/example/coupon/application/CouponService.java
+++ src/main/java/com/example/coupon/application/CouponService.java
@@ -2,18 +2,59 @@ package com.example.coupon.application;
 
 import com.example.coupon.domain.Coupon;
 import com.example.coupon.domain.CouponRepository;
+import com.example.coupon.domain.CouponStock;
+import com.example.coupon.domain.CouponStockRepository;
 import com.example.coupon.dto.request.CouponDeleteRequest;
 import com.example.coupon.dto.request.CouponRequest;
 import com.example.coupon.dto.request.CouponUpdateRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
 @RequiredArgsConstructor
 public class CouponService {
     private final CouponRepository couponRepository;
+    private final CouponStockRepository couponStockRepository;
+
     public Coupon createCoupon(CouponRequest couponRequest) {
-        return couponRepository.save(new Coupon());
+        // valid type
+        if(!Coupon.validateType(couponRequest.getType(), couponRequest.getCount()))
+            throw new IllegalArgumentException("Invalid coupon type");
+
+        // valid discount_type
+        if(!Coupon.validateDiscount_type(couponRequest.getDiscount_type(), couponRequest.getDiscount_amount()))
+            throw new IllegalArgumentException("Invalid discount type");
+
+        // valid start_date and end_date
+        if(!Coupon.validateDate(couponRequest.getStart_date(), couponRequest.getEnd_date()))
+            throw new IllegalArgumentException("Invalid date");
+
+        // valid expire_minute
+        if(!Coupon.validateExpireMinute(couponRequest.getExpireMinute()))
+            throw new IllegalArgumentException("Invalid expire minute");
+
+        Coupon coupon = new Coupon();
+        coupon.setName(couponRequest.getName());
+        coupon.setType(couponRequest.getType());
+        coupon.setCount(couponRequest.getCount());
+        coupon.setStart_date(couponRequest.getStart_date());
+        coupon.setEnd_date(couponRequest.getEnd_date());
+        coupon.setExpire_minute(couponRequest.getExpireMinute());
+        coupon.setDiscount_type(couponRequest.getDiscount_type());
+        coupon.setDiscount_amount(couponRequest.getDiscount_amount());
+        coupon.setCreated_at(LocalDateTime.now());
+        coupon.setUpdated_at(LocalDateTime.now());
+        Coupon couponResponse = couponRepository.save(coupon);
+
+        CouponStock couponStock = new CouponStock();
+        couponStock.setCoupon_id(couponResponse.getId());
+        couponStock.setCount(couponRequest.getCount());
+        couponStock.setCreated_at(LocalDateTime.now());
+        couponStock.setUpdated_at(LocalDateTime.now());
+        couponStockRepository.save(couponStock);
+        return couponResponse;
     }
 
     public Coupon updateCoupon(CouponUpdateRequest couponUpdateRequest){
--- src/main/java/com/example/coupon/domain/Coupon.java
+++ src/main/java/com/example/coupon/domain/Coupon.java
@@ -17,12 +17,29 @@ public class Coupon {
     private String name;
     private int type;
     private int count;
-    private LocalDateTime Start_date;
-    private LocalDateTime End_date;
-    private int expireMinute;
+    private LocalDateTime start_date;
+    private LocalDateTime end_date;
+    private int expire_minute;
     private int discount_type;
     private double discount_amount;
     private LocalDateTime created_at;
     private LocalDateTime updated_at;
     private LocalDateTime deleted_at;
+
+    public static boolean validateType(int type, int count){
+        return type == 0 && count == 0 || type == 1 && count > 0;
+    }
+
+    public static boolean validateDiscount_type(int discount_type, double discount_amount){
+        return discount_type == 0 && discount_amount > 0
+                || discount_type == 1 && discount_amount > 0 && discount_amount <= 100;
+    }
+
+    public static boolean validateDate(LocalDateTime startDate, LocalDateTime endDate){
+        return startDate.isAfter(LocalDateTime.now()) && startDate.isBefore(endDate);
+    }
+
+    public static boolean validateExpireMinute(int expireMinute){
+        return expireMinute > 0;
+    }
 }
