d37ef3030be348dd8195303445a81af08a612112
seungwonlee003 <seungwonlee03@icloud.com>
2024-04-15 18:58:16 +0900

    rough draft of unit test for create coupon endpoint

--- src/test/java/com/example/coupon/application/CouponServiceTest.java
+++ src/test/java/com/example/coupon/application/CouponServiceTest.java
@@ -18,6 +18,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 public class CouponServiceTest {
+
     @Mock
     private CouponRepository couponRepository;
 
